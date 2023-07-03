53a8b77995b2fea901f805db1d639d77a1b2f87c
ritullia <brigrita@gmail.com>
2023-07-03 15:10:20 +0300

    main page & styled components commit

---
+++ my-portfolio/src/componentStyle/MainPageStyled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  margin: 80px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background: linear-gradient(#e9eaf0, #cdd2d6);
+  width: auto;
+  height: 70vh;
+  border-radius: 35px;
+  box-shadow: 8px 10px 30px #dddddd;
+`;
+
+export const MainImg = styled.img`
+  width: 740px;
+`;
--- my-portfolio/src/components/MainPage.jsx
+++ my-portfolio/src/components/MainPage.jsx
@@ -1,9 +1,17 @@
+import { MainContainer, MainImg } from "../componentStyle/MainPageStyled";
+import working from "../assets/working.png";
+
 export const MainPage = () => {
   return (
     <>
-      <div>
-        <h1>Main Page Here</h1>
-      </div>
+      <MainContainer>
+        <MainImg src={working} alt="" />
+
+        <div>
+          <h1>Hello,</h1>
+          <h1>I'M RITA CINCILEVIČIENĖ</h1>
+        </div>
+      </MainContainer>
     </>
   );
 };
